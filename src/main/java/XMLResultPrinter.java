7b296429863a64562cd92a5bf4d3d7d3fe9663c7
Emiliano Suárez <emilianosuarez@gmail.com>
2013-11-13 00:51:32 -0300

    Actualizacion de la clase que genera el XML

--- src/main/java/XMLResultPrinter.java
+++ src/main/java/XMLResultPrinter.java
@@ -1,6 +1,7 @@
 package main.java;
 
 import java.io.File;
+import java.util.HashMap;
 
 import javax.management.Attribute;
 import javax.xml.parsers.DocumentBuilder;
@@ -23,8 +24,9 @@ public class XMLResultPrinter extends ResultPrinter {
 	
 	private Document document;
 	private Element rootElement;
-	private Element previousElement;
 	private String fileName = "file.xml";
+	private HashMap<Integer, Element> treeElements = new HashMap<Integer, Element>();
+	private Integer level;
 	
 	public XMLResultPrinter() throws ParserConfigurationException {
 		
@@ -35,31 +37,54 @@ public class XMLResultPrinter extends ResultPrinter {
 		this.document = docBuilder.newDocument();
 		this.rootElement = document.createElement("UnitTest");
 		document.appendChild(rootElement);
-		this.previousElement = this.rootElement;
+		//this.previousElement = this.rootElement;
+		this.level = 0;
+		addElement(rootElement);
 	}
-
+	
+	private void addElement(Element element) {
+		treeElements.put(level, element);
+		this.level++;
+	}
+	
+	private Element getElement(Integer level) {
+		while ("testcase" == treeElements.get(level).getNodeName()) {
+			level--;
+		}
+		return treeElements.get(level);
+	}
+	
 	public static XMLResultPrinter getInstance() throws ParserConfigurationException {
 		if(instance == null) {
 			instance = new XMLResultPrinter();
 		}
 		return instance;
 	}
-	
+
 	public void printTest(Test test) {
 		Element testElement = document.createElement("testcase");
 		testElement.setAttribute("name", test.getName());
 		Attr attr = document.createAttribute("result");
 		attr.setValue(test.getResult().getCode());
 		testElement.setAttributeNode(attr);
-		this.previousElement.appendChild(testElement);
+		
+		Integer level = this.level;
+		level--;
+		Element previousElement = getElement(level);
+		previousElement.appendChild(testElement);
+		addElement(testElement);
 	}
 	
 	@Override
 	public void printSuite(TestSuite suite) {
 		Element testSuiteElement = document.createElement("testsuite");
 		testSuiteElement.setAttribute("name", suite.getName());
-		this.previousElement.appendChild(testSuiteElement);
-		this.previousElement = testSuiteElement;
+
+		Integer level = this.level;
+		level--;
+		Element previousElement = getElement(level);
+		previousElement.appendChild(testSuiteElement);
+		addElement(testSuiteElement);
 	}
 
 	@Override
