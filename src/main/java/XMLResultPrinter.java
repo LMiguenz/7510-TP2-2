fc8b07eb690116b070a40df6bcd52dbe20bc8fdc
Emiliano Suárez <emilianosuarez@gmail.com>
2013-11-14 00:30:24 -0300

    Removed unused dependencies in XMLResultPrinter class

--- src/main/java/XMLResultPrinter.java
+++ src/main/java/XMLResultPrinter.java
@@ -3,12 +3,10 @@ package main.java;
 import java.io.File;
 import java.util.HashMap;
 
-import javax.management.Attribute;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
